f83ab93ef630a3882d4b09f62e12f41a7001f97f
Faiaz30 <47560573+Faiaz30@users.noreply.github.com>
2022-02-01 23:13:31 +0600

    Create Fact use.py

---
+++ Fact use.py
@@ -0,0 +1,36 @@
+
+import math
+n = 5
+result = math.factorial(n)
+print(result)
+
+
+# solve this problem  by using function
+
+'''def fact(n):
+    a = 1
+    for i in range(1, n+1):
+        a = a * i
+    return a
+result = fact(6)
+print("factorial number ans: ", result)'''
+
+# solve this problem  by using for loop
+
+
+# n = int(input('Enter any fact value: '))
+# x = 1 
+
+
+'''for i in range(1, n+1):
+    x = x * i
+print("factorial number ans: ", x)'''
+
+# solve this problem  by using for loop
+
+'''i = 1
+while i <= n:
+    x = x * i
+    i = i + 1
+
+print("factorial of ", x)'''
